7afe8955717a4a1b3d78a630b023a848b79a2aeb
David Enos <bluedangerforyou@gmail.com>
2016-03-21 23:26:54 -0700

    Added Geo location
    You will need account from wigle.net and add to the script
    You will also need to have mechanize using 
    pip install mechanize

--- wifispy.py
+++ wifispy.py
@@ -1,3 +1,9 @@
+import os
+import optparse
+import mechanize
+import urllib
+import re
+import urlparse
 from _winreg import *
 
 def val2addr(val):
@@ -6,6 +12,26 @@ def val2addr(val):
 		addr += '%02x '% ord(ch)
 	addr = addr.strip(' ').replace(' ', ':')[0:17]
 	return addr
+def wiglePrint(username, password, netid):
+	browser = mechanize.Browser()
+	browser.open('http://wigle.net')
+	reqData = urllib.urlencode({'credential_0': username, 'credential_1': password})
+	browser.open("https://wiglenet.net//gps/gps/main/login", reqData)
+	params = {}
+	params['netid'] = netid
+	reqParams = urllib.urlencode(params)
+	respURL = 'http://wigle.net/gps/gps/main/confirmquery/'
+	resp = browser.open(respURL, reqParams).read()
+	mapLat = 'N/A'
+	mapLon = 'N/A'
+	rLat = re.findall(r'maplat=.*\&', resp)
+	if rLat:
+		mapLat = rLat[0].split('&')[0].split('=')[1]
+	rLon = re.findall(r'maplon=.*\&', resp)
+	if rLon:
+		mapLon = rLon[0].split
+		print '[-] Lat: ' + mapLat + ', Lon: ' + mapLon
+				
 def printNets():
 	net = "SOFTWARE\MICROSOFT\WINDOWS NT\CurrentVersion"+\
 		"\NetworkList\Signatures\Unmanaged"
