0b9ef1f33cf2e271989cc7c1be92c8cf5b399fe5
David Enos <bluedangerforyou@gmail.com>
2016-03-21 23:29:43 -0700

    Update wifispy.py

--- wifispy.py
+++ wifispy.py
@@ -16,11 +16,11 @@ def wiglePrint(username, password, netid):
 	browser = mechanize.Browser()
 	browser.open('http://wigle.net')
 	reqData = urllib.urlencode({'credential_0': username, 'credential_1': password})
-	browser.open("https://wiglenet.net//gps/gps/main/login", reqData)
+	browser.open("https://wiglenet.net//gps//gps//main//login", reqData)
 	params = {}
 	params['netid'] = netid
 	reqParams = urllib.urlencode(params)
-	respURL = 'http://wigle.net/gps/gps/main/confirmquery/'
+	respURL = 'http://wigle.net//gps//gps//main//confirmquery/'
 	resp = browser.open(respURL, reqParams).read()
 	mapLat = 'N/A'
 	mapLon = 'N/A'
