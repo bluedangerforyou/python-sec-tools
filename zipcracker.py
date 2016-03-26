c612bb89d5414a1f0aa34ba9676116b06559556c
David Enos <bluedangerforyou@gmail.com>
2016-03-26 00:23:09 -0700

    Update zipcracker.py

--- zipcracker.py
+++ zipcracker.py
@@ -1,15 +1,27 @@
 import zipfile
-zipperfile = raw_input("Please enter zip file to crack : ")
-zFile = zipfile.ZipFile(zipperfile)
-dicfile = raw_input("Please enter your dictionary file: ")
-passFile = open(dicfile)
-for line in passFile.readlines():
-	password = line.strip('\n')
-	print("Cracking zip...")
-	try:
-		zFile.extractall(pwd=password)
-		print '[+] Password = ' + '\n'
-		exit(0)
-	except Exception, e:
-		pass
-
+from threading import Thread
+def extractFile(zFile, password):
+    try:
+        zFile.extractall(pwd=password)
+        print '[+] Found Password ' + password + '\n'
+    except:
+        pass
+def main():
+    zipperfile = raw_input("Please enter zip file to crack : ")
+    zFile = zipfile.ZipFile(zipperfile)
+    dicfile = raw_input("Please enter your dictionary file: ")
+    passFile = open(dicfile)
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        print("Cracking zip...")
+        try:
+            zFile.extractall(pwd=password)
+            print '[+] Password = ' + password + '\n'
+            t = Thread(target=extractFile, args=(zFile, password))
+            t.start()
+            raw_input()
+            exit(0)
+        except Exception, e:
+                pass
+if __name__ == '__main__':
+    main()
