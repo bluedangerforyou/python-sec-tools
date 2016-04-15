c125e4dd633656e268f6daf3c1264458622217f4
David Enos <bluedangerforyou@gmail.com>
2016-04-15 23:32:47 +0000

    Update pyprobe.py

--- pyprobe.py
+++ pyprobe.py
@@ -1,11 +1,11 @@
 from scapy.all import *
-interface = 'mon0'
-probeReqs = []
-def probesniff(p):
+
+interface = raw_input("Enter your monitor mode interface- i.e wlan0mon: ")
+probeReq = []
+def probsniff(p):
     if p.haslayer(Dot11ProbeReq):
         netName = p.getlayer(Dot11ProbeReq).info
-        if netName not in probeReqs:
+        if netName not in probeReq:
             probeReq.append(netName)
             print '[+] We detected a new probe request: ' + netName
 sniff(iface=interface, prn=probsniff)
-    
